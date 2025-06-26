a929f189f844145258ce92294e91556504328dab
jiwonWP <bread0264@gmail.com>
2025-06-26 16:51:57 +0900

    WIP: step3-temp 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalResetStyle } from './styles/reset'
 import { Global } from "@emotion/react";
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme/theme';
 import GiftMain from './pages/GiftMain';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalResetStyle} />
-          <GiftMain />
+          <Routes>
+          <Route path="/" element={<GiftMain />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
     </ThemeProvider>
   );
 };
